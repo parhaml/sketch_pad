e453d2bb31e70f1a7fd44b373630049fb527b163
Lee Parham <lee.p.parham@gmail.com>
2015-10-26 20:52:46 +0800

    functioning reset button

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 var size = 16;
-var dimension = (600-size)/size;
 var draw = function(size){
+	var dimension = (600-size)/size;
 	for (i = 1; i <= size; i++) {	
  		$('#wrapper').append("<div class='outer' id=outer"+i+"></div>");   
  		for (j=1; j<= size; j++) {
 		$("#outer"+i).append("<div class='inner' id="+j+"></div>");
 		}
 	}
+	$('.inner').css({"width": dimension, "height": dimension});
+	$('.inner').hover(function(){
+		$(this).css("background-color", "#888");
+	});
 }
 
 
 draw(size)
-$('.inner').css({"width": dimension, "height": dimension});
 
-$('.inner').hover(function(){
-	$(this).css("background-color", "#888");
+
+
+
+$('#button').click(function(){
+	var size = prompt("How big should the grid be?");
+	$('.inner').remove();
+	$('.outer').remove();
+	draw(size);
 });
-
 
 /*var size = prompt("How big should the grid be?");*/
